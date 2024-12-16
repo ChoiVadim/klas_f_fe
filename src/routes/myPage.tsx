b107978d6725c7b8642d739c572e760c0acef21d
Tsoi Vadim <tsoivadim97@gmail.com>
2024-12-16 17:15:10 +0900

    moved

--- src/routes/myPage.tsx
+++ src/routes/myPage.tsx
@@ -1,6 +1,9 @@
+import Subjects  from "../components/subjects";
+import ElectiveS from "../components/elective";
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
 import ParticlesBackground from "../components/ui/ParticlesBackground";
+
 
 export default function MyPage() {
     return (
@@ -16,13 +19,25 @@ export default function MyPage() {
             <div className="flex-1 bg-background overflow-y-auto ml-4 md:ml-0 items-center align-center p-4" id="main-content">
                 <div className="flex flex-col md:ml-16 md:mt-16">
                     <h1 className="text-6xl font-bold text-card-foreground">
-                        Chat
+                        Subjects
                     </h1>
                     <h2 className="font-medium mb-8 mr-10">
-                        Chat with your friends! 
-                    </h2>   
-                    <div className="grid grid-cols-2 md:grid-cols-5 gap-4 md:gap-8 md:mr-16">
-                        <div className="bg-card p-6 rounded-3xl shadow-md flex items-center aspect-square max-h-96 z-10">
+                        All major and elective subjects what you need in one place! 🖐 
+                    </h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-20 md:mr-16">
+                        {/* Major Subjects */}
+                        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:mr-16">
+                            <h1 className="col-span-2 md:col-span-3 text-xl font-bold text-card-foreground">
+                                Major Subjects
+                            </h1>
+                            <Subjects />
+                        </div>
+                        {/* Elective Subjects */}
+                        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:mr-16">
+                            <h1 className="col-span-2 md:col-span-3 text-xl font-bold text-card-foreground">
+                                Elective Subjects
+                            </h1>
+                            <ElectiveS />
                         </div>
                     </div>
                 </div>
